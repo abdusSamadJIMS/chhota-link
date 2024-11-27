cd1dab9b14abbc2234d13a9f99b27444e7777bd1
abdusSamadJIMS <146073969+abdusSamadJIMS@users.noreply.github.com>
2024-11-27 14:00:08 +0000

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,9 +57,64 @@ const workSans = localFont({
   variable: "--font-work-sans",
 
 });
+
+const baseUrl = process.env.BASE_URL!
 export const metadata: Metadata = {
   title: "Chhota Link | Create short link out of big ones",
   description: "Easy, simple, free and fast link shortner | Chhota Link",
+  icons: [
+    {
+      rel: "icon",
+      href: "/favicon-96x96.png",
+      sizes: "96x96",
+      url: `${baseUrl}/favicon-96x96.png`,
+      type: "image/png"
+    },
+    {
+      rel: "icon",
+      href: "/favicon.svg",
+      url: `${baseUrl}/favicon.svg`,
+      type: "image/svg+xml"
+    },
+    {
+      rel: "shortcut icon",
+      href: "/favicon.ico",
+      sizes: "96x96",
+      url: `${baseUrl}/favicon.ico`,
+    },
+    {
+      rel: "apple-touch-icon",
+      href: "/apple-touch-icon.png",
+      sizes: "180x180",
+      url: `${baseUrl}/apple-touch-icon.png`,
+    },
+  ],
+  manifest: `${baseUrl}/site.webmenifest`,
+  alternates: {
+    canonical: baseUrl,
+  },
+  openGraph: {
+    type: "website",
+    images: {
+      url: `${baseUrl}/opengraph-image.png`,
+      width: 1200,
+      height: 630,
+    },
+    siteName: "Chhota Link",
+    title: "Chhota Link | Create short link out of big ones",
+    description: "Easy, simple, free and fast link shortner | Chhota Link",
+    url: baseUrl
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "Abdus Samad",
+    images: {
+      url: `${baseUrl}/opengraph-image.png`
+    },
+    site: "Chhota Link",
+    description: "Easy, simple, free and fast link shortner | Chhota Link",
+  }
+
 };
 
 export default function RootLayout({
